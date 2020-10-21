d8a9da617fc526b7e54c338b9be78e988416d1b8
saadati944 <saadati944@gmail.com>
2020-10-21 12:59:14 +0330

    seperate main database from reports database

--- db.py
+++ db.py
@@ -1,12 +1,29 @@
-import json
+import json,os
 db={}
+rdb={}
 
 def load():
-    global db
+    global db,rdb
     with open('db','r',encoding='utf-8') as f:
         db=json.loads(f.read())
+    if not os.path.exists('rdb'):
+        generateRDB()
+    else:
+        with open('rdb','r',encoding='utf-8') as f:
+            rdb=json.loads(f.read())
 
 def dump():
-    global db
-    with open('db','w',encoding='utf-8') as f:
-        json.dump(db,f)
+    global rdb
+    with open('rdb','w',encoding='utf-8') as f:
+        json.dump(rdb,f)
+
+def generateRDB():
+    global db,rdb
+    for k in db.keys():
+        #v=views count , d=difficulty
+        rdb[k]={'v':0,'d':0}
+
+
+if __name__=='__main__':
+    load()
+    dump()
