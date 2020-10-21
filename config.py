88e4476c4af2c1faecb3fabc761253deb4ad1929
saadati944 <saadati944@gmail.com>
2020-10-21 16:28:32 +0330

    add config.py file to store configuration files

---
+++ config.py
@@ -0,0 +1,14 @@
+#this is the configuration file
+
+words_in_each_discover=5
+words_in_each_practice=5
+
+max_practices_for_each_word=5
+
+message_for_ending_practicing=""":D  you don't have any words to practice.
+
+press enter to continue..."""
+
+message_for_ending_practicing=""":D  you don't have any words to discover.
+
+press enter to continue..."""
