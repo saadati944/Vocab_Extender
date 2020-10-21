b9b5e7cd7478d5953bcd0cb54cda4b379cd1def9
saadati944 <saadati944@gmail.com>
2020-10-21 16:13:10 +0330

    add discover function to discover new words

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import db
+from os import system,name
+
+def clear():
+    _= system('cls' if name=='nt' else 'clear')
 
 db.load()
 
+def discover(count=5):
+    words=db.getnewword(count,True)
+    i=1
+    for w in words:
+        clear()
+        print(f"{i} :   {w}")
+        print("\n",db.db[w])
+        input('\npress enter to continue...')
+        i+=1
+
+
+
 while True:
-    for w in db.getnewword(int(input('count : '))):
-        print(w)
-    print()
-    print()
+    discover()
